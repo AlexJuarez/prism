2f23332d012da08439173ce8a319fcf2276c51df
Alex Juarez <alex.juarez@mheducation.com>
2018-11-08 16:22:12 -0800

    updated config

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { run } = require('./src');
-const path = require('path');
-const preferArrowFn = require('./src/transforms/preferArrowFn');
-const preferConstLet = require('./src/transforms/preferConstLet');
+// const path = require('path');
+// const preferArrowFn = require('./src/transforms/preferArrowFn');
+// const preferConstLet = require('./src/transforms/preferConstLet');
 
-run(path.join(__dirname, 'src/transforms/__fixtures__/preferArrowFn.input.js'), [preferArrowFn], { dryRun: true });
-// run('~/projects/dle-course-assembly/app/**/*.ts', [preferArrowFn, preferConstLet], { dryRun: true, summary: false });
+// run(path.join(__dirname, 'src/transforms/__fixtures__/preferArrowFn.input.js'), [preferArrowFn], { dryRun: true });
+run('~/projects/dle-course-assembly/app/**/*.ts', [preferArrowFn], { dryRun: false, summary: false });
